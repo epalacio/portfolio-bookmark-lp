ea23f4a7479c98167f5ac5744a909ebfee9b5161
Esteban Palacio <estipalacio@gmail.com>
2021-09-22 11:23:30 -0400

    Added main setup for project. Removed unnecesary files/code, added custom colours and font

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    color: {
+      royalBlue: '#5267df',
+      carnation: '#fa5959',
+      valhalla: '#242a45',
+      graySuit: '#9194a2'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
