a54965109e891d32299caee6b6d9c6dd516345ad
Esteban Palacio <estipalacio@gmail.com>
2021-09-25 17:14:23 -0400

    fix hero X-overflow on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Link} from 'react-router-dom';
 
 import illustrationHero from '../img/illustration-hero.svg'
 
 const Hero = () => {
     return (
-        <Router>
         <div id='hero' className='flex items-center flex-col-reverse lg:flex-row text-center lg:text-left p-4'>
-            <div>
+            <div className='w-full'>
                 <h1 className='text-headerMobile md:text-headerDesktop text-valhalla font-medium'>A Simple BookMark Manager</h1>
                 <p className='text-textMobile md:text-textDesktop text-graySuit my-12'>A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
                 <div className='flex justify-center md:justify-start'>
-                    <Link className='text-textCTA block bg-royalBlue hover:bg-white rounded w-max border-2 border-transparent hover:border-royalBlue text-white hover:text-royalBlue font-medium px-5 py-3 min-w-ctaWidth mr-3 shadow' to='/'>Get it on Chrome</Link>
-                    <Link className='text-textCTA block bg-blackHaze hover:bg-white rounded w-max border-2 border-transparent hover:border-valhalla text-valhalla font-medium px-5 py-3 min-w-ctaWidth shadow' to='/'>Get it on Firefox</Link>
+                    <a className='text-textCTA block bg-royalBlue hover:bg-white rounded w-max border-2 border-transparent hover:border-royalBlue text-white hover:text-royalBlue font-medium px-5 py-3 min-w-ctaWidth mr-3 shadow' href='/'>Get it on Chrome</a>
+                    <a className='text-textCTA block bg-blackHaze hover:bg-white rounded w-max border-2 border-transparent hover:border-valhalla text-valhalla font-medium px-5 py-3 min-w-ctaWidth shadow' href='/'>Get it on Firefox</a>
                 </div>
             </div>
             <div id='heroBluePill'>
                 <img className='m-auto max-w-imgWidthMobile sm:max-w-heroImgWidthDesktop' src={illustrationHero} alt=''/>
             </div>
         </div>
-        
-        </Router>
-        
      );
 }
  
